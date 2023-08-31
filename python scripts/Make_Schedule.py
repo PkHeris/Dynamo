02e6b0b60a3e6301711c6d6e91e9c32748553c08
PkHeris <pkazemzadehheris@uwaterloo.ca>
2023-08-31 14:49:46 -0700

    new

--- python scripts/Make_Schedule.py
+++ python scripts/Make_Schedule.py
@@ -8,6 +8,11 @@ clr.AddReference('RevitServices')
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
+# Function to rename field titles
+def rename_field(schedule_definition, field_ids, old_name, new_name):
+    field = schedule_definition.GetField(field_ids[old_name].IntegerValue)
+    field.ColumnHeading = new_name
+
 # Get the current document
 doc = DocumentManager.Instance.CurrentDBDocument
 
@@ -20,6 +25,9 @@ categoryId = Category.GetCategory(doc, BuiltInCategory.OST_FabricationPipework).
 # Create a new schedule for MEP Fabrication Pipework
 schedule = ViewSchedule.CreateSchedule(doc, categoryId)
 
+# Rename the schedule's view name to the desired assembly name
+schedule.Name = "NSB-P4-SUBS5-STM-55"
+
 # Get ScheduleDefinition from ViewSchedule
 scheduleDef = schedule.Definition
 
@@ -27,7 +35,7 @@ scheduleDef = schedule.Definition
 schedulableFields = scheduleDef.GetSchedulableFields()
 
 # List of desired field names
-fields = ["Assembly Name", "Item Number", "Family", "Size", "Count", "Length", "MN-FP Connector 1", "MN-FP Connector 2"]
+fields = ["Assembly Name", "Item Number", "Family", "Size", "Count", "Length", "MN-FP_Connector 1", "MN-FP_Connector 2"]
 
 # Create a dictionary for quick lookup of SchedulableFields by their name
 fields_dict = {sf.GetName(doc): sf for sf in schedulableFields}
@@ -49,7 +57,18 @@ sortFamily = ScheduleSortGroupField(field_ids["Family"], ScheduleSortOrder.Ascen
 scheduleDef.AddSortGroupField(sortItemNumber)
 scheduleDef.AddSortGroupField(sortFamily)
 
+# Rename the necessary fields
+rename_field(scheduleDef, field_ids, "Family", "Description")
+rename_field(scheduleDef, field_ids, "Count", "Qty")
+rename_field(scheduleDef, field_ids, "Length", "Length (mm)")
+rename_field(scheduleDef, field_ids, "MN-FP_Connector 1", "End Prep 1")
+rename_field(scheduleDef, field_ids, "MN-FP_Connector 2", "End Prep 2")
+
+# Add filter for the Assembly Name
+filter = ScheduleFilter(field_ids["Assembly Name"], ScheduleFilterType.Equal, "NSB-P4-SUBS5-STM-55")
+scheduleDef.AddFilter(filter)
+
 # Close the transaction
 TransactionManager.Instance.TransactionTaskDone()
 
-OUT = fields_dict
+OUT = schedule
